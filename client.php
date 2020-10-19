210ee6f57da32d8061e57cb4d67d3639e14a02d0
Kuol Kuol <36291007+KuolKuol@users.noreply.github.com>
2020-10-18 19:25:22 -0600

    Update client.php

--- client.php
+++ client.php
@@ -7,8 +7,8 @@
 
     $woocommerce = new Client(
     'https://flamecitygrillz.ca/api', // Your store URL
-    'ck_8f508793e00f6d3aeea1fa7150a9d29d42773997', // Your consumer key
-    'cs_816cc626553a2ade27ee2263f28309eb428db9e2', // Your consumer secret
+    '', // Your consumer key
+    '', // Your consumer secret
         [
             'wp_api' => true, // Enable the WP REST API integration
             'version' => 'wc/v3' // WooCommerce WP REST API version
